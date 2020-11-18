d3d42f13e71a3b2f83abf9a8b30e5e447bba89a2
Paula Orjuela <paulis1996nat@gmail.com>
2020-11-18 11:16:34 -0500

    Creación de componentes

--- Emotions/src/app/app.module.ts
+++ Emotions/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { InicioSesionComponent } from './Componentes/inicio-sesion/inicio-sesion.component';
+import { RegistroComponent } from './Componentes/registro/registro.component';
+import { IndexComponent } from './Componentes/index/index.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioSesionComponent,
+    RegistroComponent,
+    IndexComponent
   ],
   imports: [
     BrowserModule
